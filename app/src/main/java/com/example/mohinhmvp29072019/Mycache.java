027b443a5b66058221de26fc3d374f999c1db8d6
phatdroid94 <phatdroid94@gmail.com>
2019-10-28 18:54:13 +0700

    mycache

--- app/src/main/java/com/example/mohinhmvp29072019/Mycache.java
+++ app/src/main/java/com/example/mohinhmvp29072019/Mycache.java
@@ -10,26 +10,38 @@ public class Mycache {
     private static SharedPreferences.Editor editor = null;
     private static Mycache mycache = null;
 
-    private Mycache(Context context){
+    private Mycache(Context context) {
         sharedPreferences = context.getSharedPreferences("CacheApp", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
     }
-    public static Mycache getInstanceCache(Context context){
-        if (sharedPreferences == null){
-           mycache = new Mycache(context);
+
+    public static Mycache getInstanceCache(Context context) {
+        if (sharedPreferences == null) {
+            mycache = new Mycache(context);
         }
         return mycache;
     }
 
-    public <T> void saveValue(String key, T objects){
-        if (objects instanceof String){
-            editor.putString(key,(String) objects);
+    public <T extends Object> void saveValue(String key, T objects) {
+        if (objects instanceof String) {
+            editor.putString(key, (String) objects);
         }
-        if (objects instanceof Boolean){
-            editor.putBoolean(key,(Boolean) objects);
+        if (objects instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) objects);
         }
 
         editor.commit();
+    }
+
+    public <T extends Object> Object getValue(String key, T defaultparam) {
+        Object value = null;
+        if (defaultparam instanceof String) {
+            value = sharedPreferences.getString(key, (String) defaultparam);
+        }
+        if (defaultparam instanceof Boolean) {
+            value = sharedPreferences.getBoolean(key, (Boolean) defaultparam);
+        }
+        return value;
     }
 
 }
